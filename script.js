2f8dc86cdf4ba6679c35d083a44d35a3479214a5
BranimirSkegro <63650809+BranimirSkegro@users.noreply.github.com>
2024-04-04 13:44:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let currentlyOpenId = null;
+
 function toggleContent(id) {
   const content = document.getElementById(`content${id}`);
   const icon = document.getElementById(`icon${id}`);
+
+  if (currentlyOpenId && currentlyOpenId !== id) {
+    document.getElementById(`content${currentlyOpenId}`).classList.remove("show-text");
+    document.getElementById(`icon${currentlyOpenId}`).src = "./assets/images/icon-plus.svg";
+  }
+
+  currentlyOpenId = currentlyOpenId === id ? null : id;
 
   content.classList.toggle("show-text");
   icon.src = content.classList.contains("show-text") ? "./assets/images/icon-minus.svg" : "./assets/images/icon-plus.svg";
